427c42f130129af326e7be991ee17c15385f000e
ge83nek <ge83nek@mytum.de>
2023-07-09 15:49:17 +0200

    Webiste intitalisation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import viteLogo from "/vite.svg";
+import { useState } from "react";
+
+import reactLogo from "./assets/react.svg";
+
+import "./App.css";
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Marry me Afaq!</h1>;
+}
+
+export default App;
